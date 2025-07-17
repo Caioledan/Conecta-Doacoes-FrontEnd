beee9bbb4ef6e9780abe94fd51f91dcdd20fedad
Caio Lemos <caioledan@gmail.com>
2025-07-17 16:36:13 -0300

    fix: buttons size

--- src/components/HeaderButtons.tsx
+++ src/components/HeaderButtons.tsx
@@ -1,8 +1,8 @@
 function HeaderButtons() {
   return (
-    <div className="flex justify-center items-center h-40 gap-10">
-        <button className="bg-[var(--color-dark-green)] rounded-4xl h-15 w-45 text-2xl hover:cursor-pointer hover:shadow-lg hover:shadow-gray-400 hover:scale-105 duration-300 transform"><a href="#">Entrar</a></button>
-        <button className="bg-[var(--color-dark-green)] rounded-4xl h-15 w-45 text-2xl hover:cursor-pointer hover:shadow-lg hover:shadow-gray-400 hover:scale-105 duration-300 transform"><a href="#">Cadastrar</a></button>
+    <div className="flex justify-center items-center h-40 gap-6">
+        <button className="bg-[var(--color-dark-green)] rounded-4xl h-12 w-40 text-2xl hover:cursor-pointer hover:shadow-md hover:shadow-green-600 hover:scale-105 duration-300 transform"><a href="#">Entrar</a></button>
+        <button className="bg-[var(--color-dark-green)] rounded-4xl h-12 w-40 text-2xl hover:cursor-pointer hover:shadow-md hover:shadow-green-600 hover:scale-105 duration-300 transform"><a href="#">Cadastrar</a></button>
     </div>
   );
 }
