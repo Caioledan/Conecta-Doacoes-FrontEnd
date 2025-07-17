abe02cc7ecc1c77563033227bbadac3f31d69e2f
Caio Lemos <caioledan@gmail.com>
2025-07-17 16:20:15 -0300

    fix: Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 function Logo(){
     return (
-        <img src="src\assets\logo-conecta-doações.png" alt="" className="h-30"/>
+        <img src="src\assets\logo-conecta-doações.png" alt="" className="h-40"/>
     )
 }
 
