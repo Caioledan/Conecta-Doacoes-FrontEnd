524b74805a64f328b4bcfc4211b9f4dfcce68760
Caio Lemos <caioledan@gmail.com>
2025-07-18 01:23:39 -0300

    feature: initial state of Items_card component

---
+++ src/db/items.ts
@@ -0,0 +1,19 @@
+export interface ItemForTrade {
+  id: number;
+  imageUrl: string;
+  title: string;
+  details: string[];
+  type: 'Troca' | 'Doação';
+}
+
+
+
+export const tradeItemsData: ItemForTrade[] = [
+  {
+    id: 1,
+    imageUrl: 'https://http2.mlstatic.com/D_NQ_NP_853033-MLB76888998956_062024-O-kit-3-livros-de-romance-e-assim-que-acaba-e-assim-que-comeca-verity-edicoes-de-colecionador-sucesso-arrebatador-da-autora-best-seller-colleen-hoover-e-fenmeno-de-vendas-no-brasil-capa-dura.webp',
+    title: 'Livros Colleen Hoover',
+    details: ['3 Livros', 'Semi-novos'],
+    type: 'Troca',
+  },
+];
