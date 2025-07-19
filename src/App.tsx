1ffa8c2a8bec1320c3c60c2891021a72da59224e
Caio Lemos <caioledan@gmail.com>
2025-07-19 17:19:42 -0300

    Main Page update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Donation_Items from './components/Donation_Items'
+import Header_main from './components/Header_main'
+import Trade_items from './components/Trade_Items'
 
 function App() {
  
   return (
-    null
+    <div>
+      < Header_main />
+      <div className='flex items-center justify-center flex-col my-15 gap-5'>
+        < Trade_items />
+        < Donation_Items />
+      </div>
+    </div>
   )
 }
 
