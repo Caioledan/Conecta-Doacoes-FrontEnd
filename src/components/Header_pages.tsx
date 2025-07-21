4535584e8997d73cdb2d380f78cb0ddbe4b8b661
Caio Lemos <caioledan@gmail.com>
2025-07-20 21:45:44 -0300

    feature: header pages

---
+++ src/components/Header_pages.tsx
@@ -0,0 +1,28 @@
+import { Search } from "lucide-react";
+import Dropdown from "./Dropdown";
+
+function Header_login(){
+    return (
+        <div>
+            <div className="h-30 flex justify-between p-4 items-center mx-4">
+                <a href=""><img src="src\assets\images\logo-cortada.png" alt="" className="h-20"/></a>
+                <div className="flex items-center bg-white w-90 h-12 rounded-xl shadow-md mr-4 border">
+                    <input className="placeholder-gray-800 text-gray-800 bg-transparent w-full focus:outline-none px-2 text-lg" type="text"  placeholder="Pesquisar" />
+                    <button className="hover:cursor-pointer rounded-full text-gray-800 px-2"><Search/></button>
+                </div>
+            </div>
+            <div className="bg-calm-blue h-20 flex items-center">
+                <ul className="flex items-center justify-around gap-5 w-175 h-10 text-white">
+                    < Dropdown />
+                    <li className="text-xl hover:cursor-pointer"><a href="#">Chats</a></li>
+                    <li className="text-xl hover:cursor-pointer"><a href="#">Meu perfil</a></li>
+                    <li className="text-xl hover:cursor-pointer"><a href="#">Ajuda</a></li>
+                </ul> 
+            </div>
+        </div>
+
+
+    ) 
+}
+
+export default Header_login;
