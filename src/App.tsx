43d15c8b0f9f563d0ad602a72e799eb64dbc36d8
Victor Ravel <victorcavalcante917@gmail.com>
2025-07-21 15:02:50 -0300

    Footer das telas de perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import './App.css'
-import CTA from './components/CTA'
-import Donation_Items from './components/Donation_Items'
-import Footer_main from './components/Footer_main'
-import Header_main from './components/Header_main'
-import Trade_items from './components/Trade_Items'
+import "./App.css";
+import CTA from "./components/CTA";
+import Donation_Items from "./components/Donation_Items";
+import Footer_main from "./components/Footer_main";
+import Header_main from "./components/Header_main";
+import Trade_items from "./components/Trade_Items";
 
 function App() {
- 
   return (
     <div>
-      < Header_main />
-      <div className='flex items-center justify-center flex-col my-15 gap-5'>
-        < Trade_items />
-        < Donation_Items />
+      <Header_main />
+      <div className="flex items-center justify-center flex-col my-15 gap-5">
+        <Trade_items />
+        <Donation_Items />
       </div>
-      <CTA /> 
+      <CTA />
       <Footer_main />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
