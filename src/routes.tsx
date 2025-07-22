42ee406253272d29e65f8354cadd2333cc87fde7
Caio Lemos <caioledan@gmail.com>
2025-07-22 20:11:12 -0300

    route added

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home_page from "./pages/Home_page";
 import Login_page from "./pages/Login_page";
 import Cadastro_page from "./pages/Cadastro_page";
+import Items_Page from "./pages/Items_page";
 
 function AppRoutes(){
     return (
@@ -10,6 +11,7 @@ function AppRoutes(){
                 <Route path="/" element={<Home_page/>}></Route>
                 <Route path="/login" element={<Login_page/>}></Route>
                 <Route path="/cadastro" element={<Cadastro_page/>}></Route>
+                <Route path="/items" element={<Items_Page/>}></Route>
             </Routes>
 
         </BrowserRouter>
